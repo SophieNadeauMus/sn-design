717e319c8c9d47e85157319ab7cbf855cb544299
Sophie Nadeau <sophienadeau.mus@outlook.com>
2026-03-06 16:31:41 -0500

    Ajout de commentaires

--- js/animations/a-propos.js
+++ js/animations/a-propos.js
@@ -6,19 +6,26 @@ var scrollAnimation = lottie.loadAnimation({
   path: 'lottie/scroll-anim.json',
 });
 
+// Attendre que l'animation soit chargée dans le navigateur
 scrollAnimation.addEventListener('DOMLoaded', function () {
 
-  var totalFrames = scrollAnimation.totalFrames;
+  // Calculer le nombre total d'images (frames) de l'animation
+  var totalImages = scrollAnimation.totalFrames;
 
   window.addEventListener('scroll', function () {
-    
-    var scrollTop = window.scrollY;
-    var docHeight = document.body.scrollHeight - window.innerHeight;
-    var scrollPercent = scrollTop / docHeight;
+    // Position actuelle du défilement depuis le haut de la page
+    var positionScroll = window.scrollY;
+    // Hauteur totale de défilement de la page (hauteur de la page - hauteur de la fenêtre)
+    var hauteurDoc = document.body.scrollHeight - window.innerHeight; 
+    // Progression du défilement de la page parcouru
+    // Math.max(0, ...) pour éviter les valeurs négatives
+    var progression = Math.max(0, positionScroll / hauteurDoc);
 
-    var frame = totalFrames * scrollPercent;
+    // Image correspondante à la progression du défilement
+    var imageCourante = totalImages * progression;
 
-    scrollAnimation.goToAndStop(frame, true);
+    // Arrêter l'animation sur l'image calculée
+    scrollAnimation.goToAndStop(imageCourante, true);
   });
 
 });
